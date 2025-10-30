8286d8ac2e12b86eddbcd65ad6f8fb4817016028
Chirag Shah <chiragshah2969@gmail.com>
2025-10-30 12:37:36 +0530

    Initial commit: Spring Boot project setup with MySQL and Swagger

---
+++ src/main/java/com/monkcommerce/coupon_service/MonkCommerceCouponServiceApplication.java
@@ -0,0 +1,13 @@
+package com.monkcommerce.coupon_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkCommerceCouponServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkCommerceCouponServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monkcommerce/coupon_service/MonkCommerceCouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkcommerce.coupon_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkCommerceCouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
